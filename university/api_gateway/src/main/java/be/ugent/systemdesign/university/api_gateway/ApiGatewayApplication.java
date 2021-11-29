a3f873e8a53374769324be4dd3f38fd4559e2402
Robbe Decorte <robbe.decorte@ugent.be>
2021-11-29 11:03:07 +0100

    add gateway

---
+++ university/api_gateway/src/main/java/be/ugent/systemdesign/university/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,22 @@
+package be.ugent.systemdesign.university.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.host("*").and().path("/api/curriculum/**").uri("http://localhost:2223/api/curriculum"))
+				.build();
+	}
+}
