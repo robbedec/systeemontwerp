dac277789825203d356d90312f3710bc34eb9edf
BramDB24 <debleecker.b@gmail.com>
2021-12-14 07:03:56 +0100

    docker

--- university/registration/src/main/resources/registrations.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO registration_data_model(registration_id, registration_date, email, name, first_name, date_of_birth, faculty, degree, status, is_active) VALUES (1000, '2021-12-09', 'debleecker@mail.be', 'De Bleecker', 'Bram', '1999-12-24', 'IngenieursWetenschappen', 'Informatica', 'SUBMITTED', 1);
-
