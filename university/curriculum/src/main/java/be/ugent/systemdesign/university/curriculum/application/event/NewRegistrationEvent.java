faf469a9d071948bb30ab26d9c4f49bdce3c5414
Robbe Decorte <robbe.decorte@ugent.be>
2021-12-16 21:25:44 +0100

    change event names

--- university/curriculum/src/main/java/be/ugent/systemdesign/university/curriculum/application/event/NewRegistrationEvent.java
+++ university/curriculum/src/main/java/be/ugent/systemdesign/university/curriculum/application/event/NewRegistrationEvent.java
@@ -1,6 +1,7 @@
 package be.ugent.systemdesign.university.curriculum.application.event;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -10,7 +11,12 @@ import lombok.Setter;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class NewRegistrationEvent {
 	
+	@JsonProperty("accountId")
 	private String studentNumber;
+	
+	@JsonProperty("faculty")
 	private String facultyName;
+	
+	@JsonProperty("degree")
 	private String degreeName;
 }
