bd7d33833bc54f68ae942320e8001cc47eb6962c
Robbe Decorte <robbe.decorte@ugent.be>
2021-12-22 11:13:58 +0100

    fix clean script

--- clean_jars.sh
+++ clean_jars.sh
@@ -1,1 +1,13 @@
-find . -name "pom.xml" -exec mvn clean -f '{}' \;
+#!/bin/bash
+
+cd university
+startdir=$(pwd)
+declare -a arr=("curriculum" "api_gateway" "account" "evaluation" "faculty" "invoice" "learningPlatform" "notification" "registration")
+
+for i in "${arr[@]}"
+do
+  cd "$i"
+  ./mvnw clean
+  cd "$startdir"
+done
+
